e4ee7d00449f3ed7c5cf271dece8c14a950942d3
correctcpp <correctcpp@richelbilderbeek.nl>
2018-01-13 12:57:55 +0100

    Done exercise

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+#include <iterator>
 #include <iostream>
+#include <vector>
 
+/// Echo all arguments
 int main(int argc, char* argv[])
 {
-  std::cout << argv[0];
-  for (int i=0; i!=argc; ++i)
-  {
-    std::cout << ' '  << argv[i];
-  }
+  const std::vector<std::string> args(argv, argv + argc);
+  std::copy(
+    std::begin(args),
+    std::end(args),
+    std::ostream_iterator<std::string>(std::cout," ")
+  );
   std::cout << '\n';
 }
